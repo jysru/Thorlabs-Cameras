2f31578da8b91aab114d8624363faccdbd11d16f
jysru <jysru@pm.me>
2024-03-20 12:37:22 +0100

    Update example_live.m

--- matlab/examples/example_live.m
+++ matlab/examples/example_live.m
@@ -11,10 +11,20 @@ sdk = ThorlabsCameraSDKLoader();
 cam1 = ThorlabsCamera("05564", sdk, name='Near field');
 
 %% Setup cameras
-cam1.setup("ExposureTimeUs", 9000, ROISize=512);
+cam1.setup("ExposureTimeUs", 12000, ROISize=1024);
 
 %% Arm cameras
 cam1.run()
+
+%%
+for i=1:100
+    disp(i)
+    cam1.get_snapshot("DisplayTimer",true)
+end
+
+%%
+cam1.get_snapshot("DisplayTimer",true);
+imshow(cam1.lastFrame), colorbar, caxis([0, 1000]);
 
 %% Change exposure on the fly and start live mode
 % Close figure to stop live, or press stop button
